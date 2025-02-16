5c224b02a0cec8ce97b686dd4e14461979ea2548
stevechan <chanshunli@gmail.com>
2025-02-16 21:30:50 +0800

    Add load ds db from dump indexeddb ds

--- src/cljs/hulunote/storage.cljs
+++ src/cljs/hulunote/storage.cljs
@@ -6,10 +6,27 @@
 
 (def jwt-auth (local-storage (atom {}) :jwt-auth))
 
-;; (async-save-db @db/dsdb)
+(defn info [& args] (js/console.log args))
+
+
+(defonce local-db (atom nil))
+
+(defn get-item [k cb]
+  (-> (.getItem @local-db k)
+      (.then cb)))
+
+(defn set-item [k v cb]
+  (-> (.setItem @local-db k v)
+      (.then cb)))
+
+(defn item-keys [cb]
+  (-> (.keys @local-db)
+      (.then cb)))
+
+;; Save: (async-save-db @db/dsdb)  => Load: (get-item "dsdb" (fn [v] (reset-db! (dt/read-transit-str v)) ))
 (defn async-save-db [dsdb]
   (info "start write db " (js/Date.))
-  (lc/set-item "dsdb"
+  (set-item "dsdb"
     (dt/write-transit-str dsdb)
     (fn [v]
       (info "end write db" (js/Date.)))))
