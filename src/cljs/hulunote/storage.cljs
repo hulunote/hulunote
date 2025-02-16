1b59b4826dd33479b35764dea369a5c261ff2270
stevechan <chanshunli@gmail.com>
2025-02-16 21:24:29 +0800

    Add async-save-db for dump datascript data to indexedDB

--- src/cljs/hulunote/storage.cljs
+++ src/cljs/hulunote/storage.cljs
@@ -1,6 +1,16 @@
 (ns hulunote.storage
   (:require [reagent.core :as reagent]
             [alandipert.storage-atom :refer [local-storage]]
+            [datascript.transit :as dt]
             [cognitect.transit :as t]))
 
 (def jwt-auth (local-storage (atom {}) :jwt-auth))
+
+;; (async-save-db @db/dsdb)
+(defn async-save-db [dsdb]
+  (info "start write db " (js/Date.))
+  (lc/set-item "dsdb"
+    (dt/write-transit-str dsdb)
+    (fn [v]
+      (info "end write db" (js/Date.)))))
+
