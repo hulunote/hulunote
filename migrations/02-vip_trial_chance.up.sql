d37c051545b01929f422edf79ad06bf60ba04eff
xlisp <xlisp@xlisps-MacBook-Pro.local>
2026-01-29 10:38:10 +0800

    remove backend file

--- migrations/02-vip_trial_chance.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS vip_trial_chances (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v1mc(),
-    "account_id" BIGSERIAL NOT NULL,
-    "kind" TEXT NOT NULL,
-    "chances" int4 NOT NULL DEFAULT 0,
-    "updated_at" timestamptz(6) NOT NULL DEFAULT now(),
-    CONSTRAINT "vip_trial_chances_pkey" PRIMARY KEY ("id")
-);
--- migrations/05-hulunote_bot_chat_level.up.sql
+++
@@ -1,8 +0,0 @@
--- 用于保存bot的短时间内发送消息时的层级信息，保存上一次创建的navid信息
-CREATE TABLE hulunote_bot_chat_level (
-  -- 这个是直接使用bot_uuid、group_uuid来做pk
-  id VARCHAR(36) PRIMARY KEY,
-  nav_id VARCHAR(36) NOT NULL,
-  platform TEXT NOT NULL,
-  noted_at TIMESTAMPTZ NOT NULL DEFAULT now()
-)
