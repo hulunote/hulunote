65636f59fb1154ceae8e7b4aabb7fe9cafa4d3b7
xlisp <xlisp@xlisps-MacBook-Pro.local>
2026-01-29 10:32:24 +0800

    remove backend: jdk8 & npx shadow-cljs release hulunote successful

--- src/clj/functor_api/service/admin_states.clj
+++
@@ -1,5 +0,0 @@
-(ns functor-api.service.admin-states)
-
-;; 今日封禁的用户
-(def today-banned-user-tag (atom nil))
-(def today-banned-user (atom #{}))
--- src/clj/functor_api/state/streaming.clj
+++
@@ -1,6 +0,0 @@
-(ns functor-api.state.streaming
-  (:require [clojure.core.async :as a]
-            [mount.core :as mount]
-            [functor-api.util :as u]
-            [functor-api.config :as config]))
-
