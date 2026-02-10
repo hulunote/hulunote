8bb0e932a858a763958521788d8df857c2680d14
xlisp <xlisp@xlisps-MacBook-Pro.local>
2026-02-11 00:31:05 +0800

    update build_dev.sh

--- build_dev.sh
+++ build_dev.sh
@@ -5,6 +5,6 @@ export CPPFLAGS="-I/usr/local/opt/openjdk@8/include"
 
 clojure -A:cljs -M -m shadow.cljs.devtools.cli watch hulunote
 
-# eletron dev:
+# eletron dev: cp shadow-cljs-electron.edn shadow-cljs.edn && cd ~/CljPro/hulunote-rust && cargo run
 npx shadow-cljs watch hulunote => cd electron &&   npm run start:dev
 
